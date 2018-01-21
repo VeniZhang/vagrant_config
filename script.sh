4b15697041936a99c98110fd46c8b101d85486a8
ZDQ <zdq@lenovo>
2018-01-21 21:41:12 +0800

    add -y

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-yum update
+yum update -y
 yum install vim -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -O https://raw.githubusercontent.com/Zhang-Veni/vim/master/.vimrc
