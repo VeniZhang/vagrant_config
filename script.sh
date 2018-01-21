8c4184c5c420438b845e276d2f9dbccd7d2894cf
ZDQ <zdq@lenovo>
2018-01-21 21:31:00 +0800

    add config of vim

---
+++ script.sh
@@ -0,0 +1,5 @@
+yum update
+yum install vim -y
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -O https://raw.githubusercontent.com/Zhang-Veni/vim/master/.vimrc
+
