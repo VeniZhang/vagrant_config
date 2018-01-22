2069f9642aec6232c10189ca83ff0f4a15ad32dc
zhangqiang <zhangqiang@seclab.hdu.edu.cn>
2018-01-22 07:20:32 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 sudo yum update -y
 sudo yum install vim -y
 sudo yum install ctags -y
+sudo yum install git -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -O https://raw.githubusercontent.com/Zhang-Veni/vim/master/.vimrc
 
