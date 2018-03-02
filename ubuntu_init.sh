565235a45fdc060051deb7c23e68b4e97162d13f
VeniZhang <dq.z@outlook.com>
2018-03-02 23:10:41 +0800

    Create ubuntu_init.sh

---
+++ ubuntu_init.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+sudo apt install vim -y
+sudo apt install ctags -y
+sudo apt install git -y
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -O https://raw.githubusercontent.com/Zhang-Veni/vim/master/.vimrc
+
