4f47f128e0b3a3f688dbf00327fb4c36fdffdedb
VeniZhang <dq.z@outlook.com>
2018-03-02 23:25:45 +0800

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -1,7 +1,6 @@
 sudo apt update -y
-sudo apt install vim -y
-sudo apt install ctags -y
-sudo apt install git -y
+sudo apt upgrade -y
+sudo apt install vim ctags git curl  -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -O https://raw.githubusercontent.com/Zhang-Veni/vim/master/.vimrc
 
