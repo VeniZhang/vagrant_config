cd3fda6b2fdc9effc83f44c2f08841ad8ce8e461
VeniZhang <dq.z@outlook.com>
2018-03-03 00:33:53 +0800

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -3,4 +3,5 @@ sudo apt upgrade -y
 sudo apt install vim ctags git curl  -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -O https://raw.githubusercontent.com/Zhang-Veni/vim/master/.vimrc
-
+sudo apt install python-pip -y
+sudo apt install python3-pip -y
