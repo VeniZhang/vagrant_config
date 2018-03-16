8e8c27f51efe593799858a4f1aa1249203151aa0
VeniZhang <dq.z@outlook.com>
2018-03-16 14:53:00 +0800

    Update centos_init.sh

--- centos_init.sh
+++ centos_init.sh
@@ -1,7 +1,8 @@
+sudo yum -y install epel-release
 sudo yum update -y
 sudo yum install vim -y
 sudo yum install ctags -y
 sudo yum install git -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -O https://raw.githubusercontent.com/Zhang-Veni/vim/master/.vimrc
-
+vim +PluginInsatll +qall
