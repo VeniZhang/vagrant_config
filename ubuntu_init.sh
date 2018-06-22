032c8bf8caad319980f955491daced34bf0fadc7
VeniZhang <dq.z@outlook.com>
2018-06-22 21:58:32 +0800

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -6,3 +6,8 @@ curl -O https://raw.githubusercontent.com/Zhang-Veni/vim/master/.vimrc
 vim +PluginInstall +qall
 sudo apt install python-pip -y
 sudo apt install python3-pip -y
+
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+sudo chsh -s /bin/zsh
+zsh
