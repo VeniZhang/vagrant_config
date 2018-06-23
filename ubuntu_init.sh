97628ebebea5538f0ecab9cf2a614f4342251c31
VeniZhang <dq.z@outlook.com>
2018-06-23 10:00:19 +0800

    Update ubuntu_init.sh

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -17,6 +17,7 @@ sudo apt install tree
 sudo apt install proxychains
 sudo apt install shadowsocks
 
+sudo apt install zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 sudo chsh -s /bin/zsh
